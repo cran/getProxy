7f9cc203b3ab39ca824eacfde5d328d4681f7cb6
Alexey Seleznev <selesnow@gmail.com>
2022-01-05 15:00:05 +0000

    version 1.13

--- R/getProxy.R
+++ R/getProxy.R
@@ -1,84 +1,203 @@
-getProxy <-
-function(country = NULL, 
-         notCountry = NULL, 
-         supportsHttps = TRUE, 
-         port = NULL, 
-         type = "http",
-         action = "start"){
+# helpers
+getGimmeproxy <- function(
+  country = NULL, 
+  notCountry = NULL, 
+  supportsHttps = TRUE, 
+  port = NULL, 
+  type = "http"
+) {
   
-
-  Sys.unsetenv("https_proxy")
-  Sys.unsetenv("http_proxy")
+  service_name <- "gimmeproxy.com"
   
+  cli_alert_info('Try to get proxy from {service_name}')
+  
+  URL_txt <- paste0("https://gimmeproxy.com/api/getProxy?",
+                    gsub("^&|&$", "",
+                         gsub("^&|&$|&{2,5}", "&",
+                              paste(if(!is.null(country)) paste0("country=",country),
+                                    if(!is.null(notCountry)) paste0("notCountry=",notCountry),
+                                    if(isTRUE(supportsHttps)) {paste0("supportsHttps=","true")}else{paste0("supportsHttps=","false")},
+                                    if(!is.null(port)) paste0("port=",port),
+                                    if(!is.null(type)) paste0("protocol=",type),
+                                    sep = "&"))))
+  
+  proxy_list_raw <- try(GET(URL_txt), silent = T)
+  proxy_list_raw <- try(content(proxy_list_raw), silent = T)
+  proxy_ip_port  <- try(proxy_list_raw$ipPort, silent = T)
+  
+  if (is.null(proxy_ip_port)) proxy_ip_port <- NA
+  
+  res <- list(
+    proxy = proxy_ip_port,
+    service = service_name,
+    success = !(is.na(proxy_ip_port)|class(proxy_ip_port)=="try-error"|proxy_ip_port==":"),
+    err_msg = proxy_list_raw$error
+  )
+  
+  return(res)
+  
+}
 
-  if(action %in% c("start","get")){
-    service_name <- "gimmeproxy.com"
+getProxyList <- function(
+  country = NULL, 
+  notCountry = NULL, 
+  supportsHttps = TRUE, 
+  port = NULL, 
+  type = "http"
+) {
+  
+  service_name <- "getproxylist.com"
+  
+  cli_alert_info('Try to get proxy from {service_name}')
+  
+  URL_txt <- paste0("https://api.getproxylist.com/proxy?",
+                    gsub("^&|&$", "",
+                         gsub("&{2,5}", "&",
+                              paste(if(!is.null(country)) paste0("country=",country),
+                                    if(!is.null(notCountry)) paste0("notCountry=",notCountry),
+                                    if(isTRUE(supportsHttps))  paste0("allowsHttps=",1),
+                                    if(!is.null(port)) paste0("port=",port),
+                                    if(!is.null(type)) paste0("protocol=",type),
+                                    sep = "&"))))
+  
+  proxy_list_raw <- try(GET(URL_txt), silent = T)
+  proxy_list_raw <- try(content(proxy_list_raw), silent = T)
+  proxy_ip_port  <- try(proxy_list_raw$ip, silent = T)
+  
+  proxy_ip_port  <- try(paste0(proxy_list_raw$ip, ":",proxy_list_raw$port))
+  
+  res <- list(
+    proxy   = proxy_ip_port,
+    service = service_name,
+    success = !(is.null(proxy_ip_port)|class(proxy_ip_port)=="try-error"|proxy_ip_port==":"),
+    err_msg = proxy_list_raw$error
+  )
+  
+  return(res)
+  
+}
 
-      packageStartupMessage("Ger proxy from gimmeproxy.com", appendLF = T)
 
-      URL_txt <- paste0("https://gimmeproxy.com/api/getProxy?",
-                        gsub("^&|&$", "",
-                        gsub("^&|&$|&{2,5}", "&",
-                        paste(if(!is.null(country)) paste0("country=",country),
-                              if(!is.null(notCountry)) paste0("notCountry=",notCountry),
-                              if(isTRUE(supportsHttps)) {paste0("supportsHttps=","true")}else{paste0("supportsHttps=","false")},
-                              if(!is.null(port)) paste0("port=",port),
-                              if(!is.null(type)) paste0("protocol=",type),
-                              sep = "&"))))
-
-      proxy_list_raw <- try(getURL(URL_txt))
-
-      proxy_ip_port <- try(fromJSON(proxy_list_raw)$ipPort, silent = T)
-      
-
-      if(is.null(proxy_ip_port)|class(proxy_ip_port)=="try-error"){
-        packageStartupMessage(paste0("Error: ",if(class(proxy_list_raw)=="character") proxy_list_raw else fromJSON(proxy_list_raw)$error,", try get proxy from getproxylist.com"), appendLF = T)
-
-        service_name <- "getproxylist.com"
-        URL_txt <- paste0("https://api.getproxylist.com/proxy?",
-                          gsub("^&|&$", "",
-                               gsub("&{2,5}", "&",
-                                    paste(if(!is.null(country)) paste0("country=",country),
-                                          if(!is.null(notCountry)) paste0("notCountry=",notCountry),
-                                          if(isTRUE(supportsHttps))  paste0("allowsHttps=",1),
-                                          if(!is.null(port)) paste0("port=",port),
-                                          if(!is.null(type)) paste0("protocol=",type),
-                                          sep = "&"))))
-
-        proxy_list_raw <- getURL(URL_txt)
-
-        proxy_ip_port <- paste0(fromJSON(proxy_list_raw)$ip, ":",fromJSON(proxy_list_raw)$port)
-      }
-      
-
-      if(is.null(proxy_ip_port)){
-        stop("Sorry, some error in both services API, try later.")
-      }else{
-      packageStartupMessage(paste0("Ger proxy from ",service_name," SUCCESS"), appendLF = T)
-      packageStartupMessage(paste0("IP port: ", proxy_ip_port), appendLF = T)
-      }
-      
-
-    if(action == "start"){
-
-        if(isTRUE(supportsHttps)){
-          Sys.setenv(https_proxy=proxy_ip_port)
-            }else{
-          Sys.setenv(http_proxy=proxy_ip_port) 
-        }
-        if(Sys.getenv("https_proxy")!=""|Sys.getenv("http_proxy")!=""){
-          packageStartupMessage("Proxy server connection SUCCES", appendLF = T)
-        }else{
-          stop("Some error in proxy start!")
-        }
-    }
-    return(proxy_ip_port)
+getProxyList2 <- function(
+  country = NULL, 
+  notCountry = NULL, 
+  supportsHttps = TRUE, 
+  port = NULL, 
+  type = "http"
+){
+  
+  service_name <- "proxy-list.download"
+  
+  cli_alert_info('Try to get proxy from {service_name}')
+  
+  proxy_list_raw <- try(GET("https://www.proxy-list.download/api/v1/get", 
+                            query = list(
+                              type = type,
+                              country = country
+                            )), silent = T)
+  
+  proxy_list_raw <- try(content(proxy_list_raw), silent = T)
+  
+  proxy_ip_port_all <- try(strsplit(proxy_list_raw, split = '\r\n', fixed = T)[[1]])
+  
+  if ( !is.null(port) ) {
+    proxy_ip_port <- proxy_ip_port_all[grepl(paste0('.*:',port,'$'), proxy_ip_port_all)][1]
+  } else {
+    proxy_ip_port <- proxy_ip_port_all[1]
   }
   
-  if(action == "stop"){
+  if (!all(grepl('\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:\\d{1,6}', proxy_ip_port))) {
+    err_msg <- proxy_ip_port
+  } else {
+    err_msg <- ""
+  }
+  
+  if (is.na(proxy_ip_port)) {
+    err_msg <- "No proxy found for your request, try changing the port"
+  }
+  
+  res <- list(
+    proxy   = proxy_ip_port,
+    service = service_name,
+    success = all(grepl('\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:\\d{1,6}', proxy_ip_port)),
+    err_msg = err_msg
+  )
+  
+  return(res)
+  
+}
 
+getProxyFunctions <- list(
+  getGimmeproxy,
+  getProxyList,
+  getProxyList2
+)
+
+# main
+getProxy <- 
+  function(country = NULL, 
+           notCountry = NULL, 
+           supportsHttps = TRUE, 
+           port = NULL, 
+           type = "http",
+           action = "start"){
+    
     Sys.unsetenv("https_proxy")
     Sys.unsetenv("http_proxy")
-    packageStartupMessage("Proxy server STOP", appendLF = T)
+    
+    if ( action %in% c("start","get") ){
+    
+      n_attempt <- 1
+      success <- FALSE
+      
+      while ( !success & n_attempt <= 10 ) {
+        
+        GPfun <- sample(getProxyFunctions, 1)[[1]]
+        proxy <- GPfun(
+          country = country,
+          notCountry = notCountry,
+          supportsHttps = supportsHttps,
+          port = port,
+          type = type
+        )
+        
+        success <- proxy$success
+        
+        if (!success) cli_alert_danger(proxy$err_msg)
+        
+        n_attempt <- n_attempt + 1
+        
+      }
+      
+      if (n_attempt == 11 & !success) {
+        cli_abort('Some error, see log!')
+      } else {
+        cli_alert_success('Get {proxy$proxy} from {proxy$service}')
+      }
+      
+      if (action == "start") {
+        
+      if( isTRUE(supportsHttps) ){
+          Sys.setenv(https_proxy=proxy$proxy)
+        }else{
+          Sys.setenv(http_proxy=proxy$proxy) 
+        }
+        if (Sys.getenv("https_proxy")!=""|Sys.getenv("http_proxy")!=""){
+          cli_alert_success('Set {proxy$proxy} SUCCESS from {proxy$service}')
+        }else{
+          cli_abort("Some error in proxy start!")
+        }
+      }
+  
+      return(proxy$proxy)
     }
+
+    if(action == "stop"){
+      
+      Sys.unsetenv("https_proxy")
+      Sys.unsetenv("http_proxy")
+      cli_alert_success("Proxy server STOP")
+      
+    }
+    
 }
